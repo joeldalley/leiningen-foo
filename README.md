7edf4e9fb40d49112ae76b82d5663e01eeec0239
Joel Dalley <joeldalley@gmail.com>
2014-03-28 07:20:00 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# foo
+
+Sample Leiningen project.
+
+## Usage
+
+Fetches a Web page using clj-http.client, and prints out the first 50 chars of the response content.
+
+## License
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject foo "0.1.0-SNAPSHOT"
+  :main foo.core
+  :description "Sample Leiningen project."
+  :url "https://github.com/technomancy/leiningen"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.9.1"]])
+
